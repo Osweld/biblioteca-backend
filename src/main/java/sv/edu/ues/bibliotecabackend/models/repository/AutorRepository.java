b0ead64fbe022ea05e5654958e2399b1328f5b80
Osweld <38056431+Osweld@users.noreply.github.com>
2024-05-14 19:29:35 -0600

    se agrego el metodo de buscar por autor

--- src/main/java/sv/edu/ues/bibliotecabackend/models/repository/AutorRepository.java
+++ src/main/java/sv/edu/ues/bibliotecabackend/models/repository/AutorRepository.java
@@ -3,5 +3,9 @@ package sv.edu.ues.bibliotecabackend.models.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sv.edu.ues.bibliotecabackend.models.entity.Autor;
 
+import java.util.List;
+
 public interface AutorRepository extends JpaRepository<Autor,Long> {
+
+    List<Autor> findByAutorContainingIgnoreCase(String nombre);
 }
