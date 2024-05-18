8fdc83a62a1d456b6cdce6adb99e56592b744aa7
Osweld <38056431+Osweld@users.noreply.github.com>
2024-05-17 22:06:56 -0600

    Se agregaron metodos

--- src/main/java/sv/edu/ues/bibliotecabackend/models/repository/MaterialRepository.java
+++ src/main/java/sv/edu/ues/bibliotecabackend/models/repository/MaterialRepository.java
@@ -1,7 +1,21 @@
 package sv.edu.ues.bibliotecabackend.models.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import sv.edu.ues.bibliotecabackend.models.entity.Material;
 
-public interface MaterialRepository extends JpaRepository<Material, Long> {
+import java.util.Optional;
+
+public interface MaterialRepository extends JpaRepository<Material, Long> ,JpaSpecificationExecutor<Material>, PagingAndSortingRepository<Material, Long> {
+
+
+    Optional<Material> findById(Long id);
+
+
+    Page<Material> findAll(Specification<Material> spec, Pageable pageable);
 }
