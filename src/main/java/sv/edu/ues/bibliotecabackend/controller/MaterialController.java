55aacc5a0a103946571734c311367ada36477816
Osweld <38056431+Osweld@users.noreply.github.com>
2024-05-17 22:08:25 -0600

    Se agrego el controlador de material

---
+++ src/main/java/sv/edu/ues/bibliotecabackend/controller/MaterialController.java
@@ -0,0 +1,51 @@
+package sv.edu.ues.bibliotecabackend.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sv.edu.ues.bibliotecabackend.models.entity.Material;
+import sv.edu.ues.bibliotecabackend.service.MaterialService;
+
+@RestController
+@RequestMapping("api/v1/material")
+public class MaterialController {
+
+    private final MaterialService materialService;
+
+    public MaterialController(MaterialService materialService) {
+        this.materialService = materialService;
+    }
+
+    @GetMapping()
+    ResponseEntity<Page<Material>> getAllMaterials(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(required = false) Long idIdioma,
+            @RequestParam(required = false) Long idAutor,
+            @RequestParam(required = false) Long idCategoria,
+            @RequestParam(required = false) Long idEstadoMaterial
+    ) {
+        return ResponseEntity.ok(
+                materialService.getAllMaterials(
+                        idIdioma,idAutor,idCategoria,idEstadoMaterial, PageRequest.of(page,size)));
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<Material> getMaterial(@PathVariable Long id) {
+        return ResponseEntity.ok(materialService.getMaterialById(id));
+    }
+
+    @PostMapping
+    ResponseEntity<Material> saveMaterial(@RequestBody @Valid Material material) {
+        return new ResponseEntity<>(materialService.saveMaterial(material), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Material> updateMaterial(@PathVariable Long id, @RequestBody @Valid Material material) {
+        return ResponseEntity.ok(materialService.updateMaterial(id, material));
+    }
+}
