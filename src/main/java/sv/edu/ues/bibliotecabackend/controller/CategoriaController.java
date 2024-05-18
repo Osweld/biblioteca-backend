325666ea339319320e0edcaa165ce65cbbfd8037
Osweld <38056431+Osweld@users.noreply.github.com>
2024-05-17 22:08:33 -0600

    Se agrego el controlador de categoria

---
+++ src/main/java/sv/edu/ues/bibliotecabackend/controller/CategoriaController.java
@@ -0,0 +1,26 @@
+package sv.edu.ues.bibliotecabackend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sv.edu.ues.bibliotecabackend.models.entity.Categoria;
+import sv.edu.ues.bibliotecabackend.models.repository.CategoriaRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/categoria")
+public class CategoriaController {
+
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaController(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    @GetMapping
+    ResponseEntity<List<Categoria>> findAll() {
+        return ResponseEntity.ok(categoriaRepository.findAll());
+    }
+}
