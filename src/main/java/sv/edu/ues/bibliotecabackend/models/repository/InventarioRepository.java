9567ccd4d9ca172f691ae92fd23f8db001c5f955
Osweld <38056431+Osweld@users.noreply.github.com>
2024-05-17 22:09:06 -0600

    Se agrego un metodo

--- src/main/java/sv/edu/ues/bibliotecabackend/models/repository/InventarioRepository.java
+++ src/main/java/sv/edu/ues/bibliotecabackend/models/repository/InventarioRepository.java
@@ -2,6 +2,11 @@ package sv.edu.ues.bibliotecabackend.models.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sv.edu.ues.bibliotecabackend.models.entity.Inventario;
+import sv.edu.ues.bibliotecabackend.models.entity.Material;
+
+import java.util.Optional;
 
 public interface InventarioRepository extends JpaRepository<Inventario, Long> {
+
+    Optional<Inventario> findInventarioByMaterial(Material material);
 }
