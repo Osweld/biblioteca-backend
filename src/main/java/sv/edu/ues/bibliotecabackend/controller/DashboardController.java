6072a2293fc7ee1fc7cf82628134824f819ea066
Osweld <38056431+Osweld@users.noreply.github.com>
2024-06-09 09:11:29 -0600

    se agrego seguridad al DashboardController

--- src/main/java/sv/edu/ues/bibliotecabackend/controller/DashboardController.java
+++ src/main/java/sv/edu/ues/bibliotecabackend/controller/DashboardController.java
@@ -2,6 +2,7 @@ package sv.edu.ues.bibliotecabackend.controller;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,21 +25,25 @@ public class DashboardController {
     }
 
     @GetMapping("/basic")
+    @Secured({"ROLE_BIBLIOTECARIO"})
     ResponseEntity<EstadisticasDTO> getEstadisticas() {
         return ResponseEntity.ok(dashboardService.getBasicStatistics());
     }
 
     @GetMapping("/prestamos-ultima-semana")
+    @Secured({"ROLE_BIBLIOTECARIO"})
     ResponseEntity<List<PrestamoDiaDTO>> getPrestamosPorDiaUltimaSemana() {
         return ResponseEntity.ok(dashboardService.getPrestamosPorDiaUltimaSemana());
     }
 
     @GetMapping("/pagos")
+    @Secured({"ROLE_BIBLIOTECARIO"})
     ResponseEntity<PagoDatosDTO> getPagos() {
         return ResponseEntity.ok(dashboardService.getPagoDatos());
     }
 
     @GetMapping("/egresos")
+    @Secured({"ROLE_BIBLIOTECARIO"})
     ResponseEntity<EgresoDatosDTO> getEgresos() {
         return ResponseEntity.ok(dashboardService.getEgresoDatos());
     }
